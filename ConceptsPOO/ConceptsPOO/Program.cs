4b481aba38152aaca1340f2107aac3b93518406b
USUARIO <USUARIO@DESKTOP-O8AI1FT>
2022-06-04 01:35:12 -0500

    Finish day class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2022, 12, 7));
+    Console.WriteLine(new Date(1974, 9, 23));
+    Console.WriteLine(new Date(1985, 11, 45));
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}
+
+
+
